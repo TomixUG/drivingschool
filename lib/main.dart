e7b687e72cc0a6e3c637a3f1d0698a26f38037b6
TomixUG <tomas.sousek1@gmail.com>
2023-05-02 00:07:09 +0200

    folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:drivingschool/screens/home.dart';
+import 'package:drivingschool/views/home_view/home_view.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: const Home(),
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
-      
-    )
-  );
+  runApp(GetMaterialApp(
+    initialRoute: '/',
+    getPages: [
+      GetPage(name: '/', page: () => const Home()),
+      // GetPage(name: '/second', page: () => Second()),
+    ],
+    theme: ThemeData(
+      colorScheme: const ColorScheme.light(),
+    ),
+    darkTheme: ThemeData(
+      colorScheme: const ColorScheme.dark(),
+    ),
+    themeMode: ThemeMode.system,
+  ));
 }
--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import '../widgets/navdrawer.dart';
-
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -14,15 +12,10 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      appBar:AppBar(
+      appBar: AppBar(
         title: const Text("Autoškola"),
       ),
       body: const Text("hello"),
     );
   }
 }
-
-
-
-
