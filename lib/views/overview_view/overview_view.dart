f8fbdc7277828939011e36b8587e29a2216b6248
TomixUG <tomas.sousek1@gmail.com>
2023-05-03 01:45:42 +0200

    add course overview card

--- lib/views/overview_view/overview_view.dart
+++ lib/views/overview_view/overview_view.dart
@@ -2,6 +2,7 @@ import 'package:drivingschool/components/shared/custom_card.dart';
 import 'package:drivingschool/views/overview_view/overview_view_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class OverviewView extends StatelessWidget {
   OverviewView({super.key});
@@ -24,15 +25,63 @@ class OverviewView extends StatelessWidget {
                   CustomCard(
                     onTap: () {},
                     child: Container(
-                      margin: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
+                      margin: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 16.0),
                       child: Column(
                         children: [
-                          const Text("Kurz Autoškola B"),
+                          Row(
+                            children: const [
+                              Icon(Icons.dashboard),
+                              SizedBox(width: 20),
+                              Text(
+                                "Kurz Autoškola B",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 12),
+                          Divider(color: Theme.of(context).dividerColor, thickness: 0.5),
+                          const SizedBox(height: 12),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: const [
-                              Text("Progress: "),
-                              Text("Progress: "),
+                            children: [
+                              Row(
+                                children: [
+                                  CircularPercentIndicator(
+                                    radius: 25.0,
+                                    lineWidth: 5.0,
+                                    percent: 0.4,
+                                    // center: new Text("100%"),
+                                    progressColor: Colors.green,
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text("Hotové: "),
+                                      Text("40 %"),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  CircularPercentIndicator(
+                                    radius: 25.0,
+                                    lineWidth: 25.0,
+                                    percent: 1,
+                                    center: new Text("10%", style: TextStyle(color: Colors.white)),
+                                    progressColor: Colors.red,
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text("Úspěšnost: "),
+                                      Text("Neprošel", style: TextStyle(color: Colors.red)),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ],
                           ),
                         ],
@@ -40,27 +89,6 @@ class OverviewView extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: CustomCard(
-                      onTap: () {},
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
-                        child: Column(
-                          children: [
-                            const Text("Kurz Autoškola B"),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text("Progress: "),
-                                Text("Progress: "),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
                 ],
               ),
             ),
