51702972756246ab3a1fb17ee12deb377a89cb63
TomixUG <tomas.sousek1@gmail.com>
2023-05-30 09:51:26 +0200

    video player should work on windows now

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:drivingschool/utils/db_controller.dart';
 import 'package:drivingschool/views/overview_view/learnquestions/learnquestions_view.dart';
 import 'package:drivingschool/views/test_view/test_view.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:drivingschool/views/home_view/home_view.dart';
 import 'package:get/get.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:video_player_win/video_player_win_plugin.dart';
 
 import 'views/overview_view/learnquestions/learnquestions_view_controller.dart';
 
@@ -21,6 +23,7 @@ void main() async {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
+  if (!kIsWeb && Platform.isWindows) WindowsVideoPlayer.registerWith();
   WidgetsFlutterBinding.ensureInitialized();
 
   // create instance of DbController (inits the db)
